e5057fba5dc4883ecf52627efb3f2cfc354e2c10
Ehsan Zilaei <zzilaei@gmail.com>
2020-01-16 14:07:55 +0100

    Story for simple test of react-router. (#2)
    - Installed react-router dependencies.

---
+++ src/components/organisms/Navigation.stories.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
+import StoryWrapper from '../molecules/StoryWrapper';
+
+storiesOf('Navigation', module).add('React router', () => (
+  <StoryWrapper>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  </StoryWrapper>
+));
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
