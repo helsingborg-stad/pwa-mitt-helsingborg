e8a398c4bb3d143777ac8af099876519fc884f54
Jonatan Hanson <jonatan.hanson@gmail.com>
2020-01-16 14:09:51 +0100

    Feature/login screen (#3)
    Fixed basic functionality for login screen, including withAuthentication logic.

---
+++ src/components/screens/LoginScreen.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import StoryWrapper from '../molecules/StoryWrapper';
+import LoginScreen from './LoginScreen'
+
+storiesOf('Screens', module)
+  .add('Login', () => (
+    <StoryWrapper>
+            <LoginScreen />
+    </StoryWrapper>
+  ));
