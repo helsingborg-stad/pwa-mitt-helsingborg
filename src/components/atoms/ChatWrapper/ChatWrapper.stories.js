f637602c3dc57c6aab76f3e235cc91fdc688ed1f
Teddy Andersson <teddyandersson@Teddys-MacBook-Pro.local>
2020-01-20 15:45:47 +0100

    Ported ChatWrapper from native app

---
+++ src/components/atoms/ChatWrapper/ChatWrapper.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ChatWrapper from './ChatWrapper';
+
+export default {
+  title: 'ChatWrapper',
+  component: ChatWrapper,
+};
+
+export const Default = () => <ChatWrapper>Hello ChatWrapper</ChatWrapper>;
