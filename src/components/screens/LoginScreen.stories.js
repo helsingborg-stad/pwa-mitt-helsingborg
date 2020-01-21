20e630eb7182bcc6c19f8a9388e86ee370dba218
Jonatan Hanson <jonatan.hanson@gmail.com>
2020-01-21 16:35:44 +0100

    Prettier formatting

--- src/components/screens/LoginScreen.stories.js
+++ src/components/screens/LoginScreen.stories.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import StoryWrapper from '../molecules/StoryWrapper';
-import LoginScreen from './LoginScreen'
+import LoginScreen from './LoginScreen';
 
-storiesOf('Screens', module)
-  .add('Login', () => (
-    <StoryWrapper>
-            <LoginScreen />
-    </StoryWrapper>
-  ));
+storiesOf('Screens', module).add('Login', () => (
+  <StoryWrapper>
+    <LoginScreen />
+  </StoryWrapper>
+));
