44ec727df5d74e77a8024bd756fe127fd5d68663
Teddy Andersson <teddyandersson@Teddys-MacBook-Pro.local>
2020-01-21 17:00:18 +0100

    Added Routing Component with config

---
+++ src/routes/routes.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { RenderRoutes } from './RouteHelpers';
+
+// TODO: REPLACE DATA WITH REAL ROUTES
+const ROUTES = [
+  { path: '/', key: 'ROOT', exact: true, component: () => <h1>Index</h1> },
+  {
+    path: '/app',
+    key: 'APP',
+    component: RenderRoutes,
+    routes: [
+      {
+        path: '/app',
+        key: 'APP_ROOT',
+        exact: true,
+        component: () => <h1>App Index</h1>,
+      },
+      {
+        path: '/app/page',
+        key: 'APP_PAGE',
+        exact: true,
+        component: () => <h1>App Page</h1>,
+      },
+    ],
+  },
+];
+
+export default ROUTES;
