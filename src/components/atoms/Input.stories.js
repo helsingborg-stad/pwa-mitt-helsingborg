b70739d62429c2add36213d8f2c2043849bd91e2
Dan Nilsson <dan.nilsson@helsingborg.se>
2020-01-21 11:17:41 +0100

    comment out empty stories

--- src/components/atoms/Input.stories.js
+++ src/components/atoms/Input.stories.js
@@ -20,21 +20,21 @@ export default {
   component: Input,
 };
 
-export const Default = () => (
-  <StoryWrapper>{/* <InputWithForm placeholder="Type something" /> */}</StoryWrapper>
-);
+// export const Default = () => (
+//   <StoryWrapper>{/* <InputWithForm placeholder="Type something" /> */}</StoryWrapper>
+// );
 
-export const Numeric = () => (
-  <StoryWrapper>
-    {/* <InputWithForm placeholder="Type some numbers" keyboardType="numeric" /> */}
-  </StoryWrapper>
-);
+// export const Numeric = () => (
+//   <StoryWrapper>
+//     {/* <InputWithForm placeholder="Type some numbers" keyboardType="numeric" /> */}
+//   </StoryWrapper>
+// );
 
-export const PhonePad = () => (
-  <StoryWrapper>
-    {/* <InputWithForm placeholder="Type some number using phone pad" keyboardType="phone-pad" /> */}
-  </StoryWrapper>
-);
+// export const PhonePad = () => (
+//   <StoryWrapper>
+//     {/* <InputWithForm placeholder="Type some number using phone pad" keyboardType="phone-pad" /> */}
+//   </StoryWrapper>
+// );
 
 export const WithOutForm = () => (
   <StoryWrapper>
