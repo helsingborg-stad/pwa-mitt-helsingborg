62969eea7ea5cfcb632e8ce0eeb8bd7568526a40
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-01-22 15:13:41 +0100

    Formatting magic

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,28 +1,28 @@
-import React from "react";
-import { RenderRoutes } from "./RouteHelpers";
+import React from 'react';
+import { RenderRoutes } from './RouteHelpers';
 
 // TODO: REPLACE DATA WITH REAL ROUTES
 const ROUTES = [
-  { path: "/", key: "ROOT", exact: true, component: () => <h1>Index</h1> },
+  { path: '/', key: 'ROOT', exact: true, component: () => <h1>Index</h1> },
   {
-    path: "/app",
-    key: "APP",
+    path: '/app',
+    key: 'APP',
     component: RenderRoutes,
     routes: [
       {
-        path: "/app",
-        key: "APP_ROOT",
+        path: '/app',
+        key: 'APP_ROOT',
         exact: true,
-        component: () => <h1>App Index</h1>
+        component: () => <h1>App Index</h1>,
       },
       {
-        path: "/app/page",
-        key: "APP_PAGE",
+        path: '/app/page',
+        key: 'APP_PAGE',
         exact: true,
-        component: () => <h1>App Page</h1>
-      }
-    ]
-  }
+        component: () => <h1>App Page</h1>,
+      },
+    ],
+  },
 ];
 
 export default ROUTES;
