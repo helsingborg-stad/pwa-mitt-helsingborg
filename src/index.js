60ec3843c82c820a08ae3e857423c46dbd716938
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-01-23 12:40:22 +0100

    Include Roboto & normalize.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css';
+import 'typeface-roboto';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
