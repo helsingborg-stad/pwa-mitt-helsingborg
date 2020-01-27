2b2c6048d59b720b18df8ce83c3259600a1c424d
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-01-27 14:25:21 +0100

    Formatting

--- src/components/molecules/StoryWrapper.js
+++ src/components/molecules/StoryWrapper.js
@@ -1,30 +1,28 @@
-import React from 'react'
-import ScreenWrapper from './ScreenWrapper';
+import React from 'react';
 import styled from 'styled-components';
+import ScreenWrapper from './ScreenWrapper';
 
 const StoryWrapper = props => (
-    <EnhancedSafeAreaView>
-        <ModifiedScreenWrapper style={props.style}>
-            {props.kind ?
-                <StoryHeading>{`${props.kind} / ${props.name}`}</StoryHeading>
-            : null}
-            {props.children}
-        </ModifiedScreenWrapper>
-    </EnhancedSafeAreaView>
+  <EnhancedSafeAreaView>
+    <ModifiedScreenWrapper style={props.style}>
+      {props.kind ? <StoryHeading>{`${props.kind} / ${props.name}`}</StoryHeading> : null}
+      {props.children}
+    </ModifiedScreenWrapper>
+  </EnhancedSafeAreaView>
 );
 
 const StoryHeading = styled.h2`
-    margin-bottom: 16px;
-    margin-top: 16px;
+  margin-bottom: 16px;
+  margin-top: 16px;
 `;
 
 const EnhancedSafeAreaView = styled.div`
-    flex: 1;
-    margin-bottom: 18px;
+  flex: 1;
+  margin-bottom: 18px;
 `;
 
 const ModifiedScreenWrapper = styled(ScreenWrapper)`
-   justify-content: flex-start;
+  justify-content: flex-start;
 `;
 
 export default StoryWrapper;
