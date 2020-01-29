33b88601032e8399ee0f544e20239998168b1c83
Jonatan Hanson <jonatan.hanson@gmail.com>
2020-01-29 16:37:23 +0100

    Created modal story

---
+++ src/components/molecules/Modal.stories.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import Button from '../atoms/Button';
+import Text from '../atoms/Text';
+import Modal from './Modal';
+import StoryWrapper from './StoryWrapper';
+
+export default {
+  component: Modal,
+  title: 'Modal',
+};
+
+class ModalExample extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      modal: {
+        visible: false,
+        heading: '',
+        content: '',
+      },
+    };
+  }
+
+  changeModal(visible, heading = '', content = '') {
+    this.setState({
+      modal: {
+        visible,
+        heading,
+        content,
+      },
+    });
+  }
+
+  render() {
+    const { modal } = this.state;
+    const { visible, heading, content } = modal;
+
+    return (
+      <div>
+        <Modal
+          color="purple"
+          visible={visible}
+          heading={heading}
+          content={content}
+          changeModal={visible => this.changeModal(visible)}
+        />
+
+        <Button
+          color="purple"
+          onClick={() =>
+            this.changeModal(
+              !visible,
+              'Modal one',
+              'Donec ullamcorper nulla non metus auctor fringilla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.'
+            )}
+        >
+          <Text>Show modal</Text>
+        </Button>
+      </div>
+    );
+  }
+}
+
+export const Default = () => (
+  <StoryWrapper>
+    <ModalExample />
+  </StoryWrapper>
+);
