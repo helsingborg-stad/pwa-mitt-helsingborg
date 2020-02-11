80059e3b8bb5c3ec2449a90b136e61fcf6090174
Ehsan Zilaei <ehsan.zilaei@helsingborg.se>
2020-02-11 21:14:37 +0100

    Upgraded from iOS 12 to iOS 13. Converted code to SwiftUI.

--- ios/MittHelsingborg/SceneDelegate.swift
+++ ios/MittHelsingborg/SceneDelegate.swift
@@ -1,14 +1,15 @@
 //
 //  SceneDelegate.swift
-//  Mitt HBG
+//  WebPagesSwiftUI
 //
-//  Created by Ehsan Zilaei on 2020-01-30.
+//  Created by Ehsan Zilaei on 2020-01-09.
 //  Copyright © 2020 Ehsan Zilaei. All rights reserved.
 //
 
 import UIKit
+import SwiftUI
 
-@available(iOS 13.0, *)
+//@available(iOS 13.0, *)
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
@@ -18,7 +19,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+
+        // Create the SwiftUI view that provides the window contents.
+        let contentView = ContentView()
+
+        // Use a UIHostingController as window root view controller.
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
